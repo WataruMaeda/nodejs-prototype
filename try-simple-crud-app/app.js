db03e49169b63d5c15e9e8b457aa30274f9034e1
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-11-14 12:35:25 -0800

    chore: add project

---
+++ try-simple-crud-app/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(morgan('short'))
+
+app.get('/', (req, res) => {
+  res.send('Hello from Rooooooooot')
+})
+
+app.get('/users', (req, res) => {
+  const user1 = {
+    id: 1,
+    name: 'test-1',
+    message: 'Hello test 1 user'
+  }
+  // res.json(user1)
+  const user2 = {
+    id: 2,
+    name: 'test-2',
+    message: 'Hello test 2 user'
+  }
+  res.json([user1, user2])
+})
+
+app.listen(3003, () => {
+  console.log('Server is up and listening in 3003 ...')
+})
