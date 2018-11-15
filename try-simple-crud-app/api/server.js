5aa22d127e30117430ef48d4d71d301bf2fa8bb4
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-11-14 17:20:52 -0800

    chore(setup): mongoDB + mongoose

--- try-simple-crud-app/api/server.js
+++ try-simple-crud-app/api/server.js
@@ -1,28 +1,41 @@
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
+const port = 3001
+const dbUrl = 'mongodb://localhost/crud' 
+const app = express()
 app.use(morgan('short'))
 
-app.get('/', (req, res) => {
-  res.send('Hello from Rooooooooot')
+mongoose.connect(dbUrl, dbErr => {
+  if (dbErr) throw new Error(dbErr)
+  else console.log('db connected')
+  
+  app.listen(port, err => {
+    if (err) throw new Error(err)
+    else console.log(`listening on port ${port}`)
+  })
 })
 
-app.get('/users', (req, res) => {
-  const user1 = {
-    id: 1,
-    name: 'test-1',
-    message: 'Hello test 1 user'
-  }
-  // res.json(user1)
-  const user2 = {
-    id: 2,
-    name: 'test-2',
-    message: 'Hello test 2 user'
-  }
-  res.json([user1, user2])
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello from Rooooooooot')
+// })
 
-app.listen(3003, () => {
-  console.log('Server is up and listening in 3003 ...')
-})
+// app.get('/users', (req, res) => {
+//   const user1 = {
+//     id: 1,
+//     name: 'test-1',
+//     message: 'Hello test 1 user'
+//   }
+//   // res.json(user1)
+//   const user2 = {
+//     id: 2,
+//     name: 'test-2',
+//     message: 'Hello test 2 user'
+//   }
+//   res.json([user1, user2])
+// })
+
+// app.listen(port, () => {
+//   console.log('Server is up and listening in 3003 ...')
+// })
