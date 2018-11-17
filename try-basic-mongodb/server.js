8436a14fd96f970c39c80dcefcd1b4b0b5362a45
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-11-17 00:21:47 -0800

    chore(connection): update db connection part and readme

--- try-basic-mongodb/server.js
+++ try-basic-mongodb/server.js
@@ -13,62 +13,62 @@ app.use(morgan('short'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-mongoose.connect(dbUrl, { useNewUrlParser: true }, dbErr => {
-  if (dbErr) throw new Error(dbErr)
-  else console.log('db connected')
+// connect MongoDB
+mongoose.connect(dbUrl)
+mongoose.connection.on('error', err => console.log('[MONGODB] unable to connect', err))
+mongoose.connection.on('open', () => console.log('[MONGODB] connected'))
 
-  // MARK: - POST
+// MARK: - POST
 
-  app.post('/api/user', (req, res) => {
-    console.log('receive POST request')
-    const { name, age } = req.body
-    if (!name || !age) return res.status(400).send('cannot')
-    new User({
-      name,
-      age
-    }).save(err => {
-      if (err) return res.status(500).send()
-      res.status(200).send(`${name}(${age}) was successfully created.`)
-    })
+app.post('/api/user', (req, res) => {
+  console.log('receive POST request')
+  const { name, age } = req.body
+  if (!name || !age) return res.status(400).send('cannot')
+  new User({
+    name,
+    age
+  }).save(err => {
+    if (err) return res.status(500).send()
+    res.status(200).send(`${name}(${age}) was successfully created.`)
   })
+})
 
-  // MARK: - GET
+// MARK: - GET
 
-  app.get('/api/user', (req, res) => {
-    User.find({}, (err, arr) => {
-      if (err) return res.status(500).send()
+app.get('/api/user', (req, res) => {
+  User.find({}, (err, arr) => {
+    if (err) return res.status(500).send()
+    res.status(200).send(arr)
+  })
+})
+
+// MARK: - PUT
+
+app.put('/api/user', (req, res) => {
+  const { id } = req.body
+  User.findByIdAndUpdate(id, { $inc: { "age": 1 } }, err => {
+    if (err) return res.status(500).send()
+    User.find({}, (findErr, arr) => {
+      if (findErr) return res.status(500).send()
       res.status(200).send(arr)
     })
   })
-  
-  // MARK: - PUT
+})
 
-  app.put('/api/user', (req, res) => {
-    const { id } = req.body
-    User.findByIdAndUpdate(id, { $inc: { "age": 1 } }, err => {
-      if (err) return res.status(500).send()
-      User.find({}, (findErr, arr) => {
-        if (findErr) return res.status(500).send()
-        res.status(200).send(arr)
-      })
+// MARK: - DELETE
+
+app.delete('/api/user', (req, res) => {
+  const { id } = req.body
+  User.findByIdAndRemove(id, err => {
+    if (err) return res.status(500).send()
+    User.find({}, (findErr, arr) => {
+      if (findErr) return res.status(500).send()
+      res.status(200).send(arr)
     })
   })
+})
 
-  // MARK: - DELETE
-
-  app.delete('/api/user', (req, res) => {
-    const { id } = req.body
-    User.findByIdAndRemove(id, err => {
-      if (err) return res.status(500).send()
-      User.find({}, (findErr, arr) => {
-        if (findErr) return res.status(500).send()
-        res.status(200).send(arr)
-      })
-    })
-  })
-
-  app.listen(port, err => {
-    if (err) throw new Error(err)
-    else console.log(`listening on port ${port}`)
-  })
+app.listen(port, err => {
+  if (err) throw new Error(err)
+  else console.log(`listening on port ${port}`)
 })
